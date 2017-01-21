7242dfe1526d3a41a6b5674f4e163ad1e88aa9f7
Xylord <xylord.khan@gmail.com>
2017-01-20 22:44:18 -0500

    Added caustics projector

---
+++ Practice/Assets/Scripts/Underwater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour {
+
+	private bool isUnderwater;
+	private Color normalColor;
+	private Color underwaterColor;
+
+	// Use this for initialization
+	void Start () {
+		normalColor = new Color(.5F, .5F, .5F, .5F);
+		underwaterColor = new Color(.22F, .65F, .77F, .5F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
