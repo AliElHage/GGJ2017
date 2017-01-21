b66b0d7e6405904196a914bd626e6673d96c78d5
AliElHage <ali.el-hage2@mail.mcgill.ca>
2017-01-21 01:32:50 -0500

    latest with bullet

--- Practice/Assets/Scripts/DolphinController.cs
+++ Practice/Assets/Scripts/DolphinController.cs
@@ -7,9 +7,11 @@ public class DolphinController : MonoBehaviour
 
 	public float speed = 10;
 	private Rigidbody2D rig;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
-	// Use this for initialization
-	void Start()
+    // Use this for initialization
+    void Start()
 	{
 
 		rig = GetComponent<Rigidbody2D>();
@@ -26,5 +28,25 @@ public class DolphinController : MonoBehaviour
 		Vector3 movement = new Vector2(0, vAxis) * speed * Time.deltaTime;
 		rig.MovePosition(transform.position + movement);
 
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = new Vector3(5F, 0, 0);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
 }
