e13a36cd332d05e23496d055c2f10024f386bf81
Xylord <xylord.khan@gmail.com>
2017-01-20 20:58:12 -0500

    Added project folder

---
+++ Practice/Assets/Scripts/DolphinController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DolphinController : MonoBehaviour {
+
+    public float speedX, forceY;
+    public Rigidbody2D playerRigidbody;
+
+    public CapsuleCollider2D playerCollider;
+
+
+	// Use this for initialization
+	void Start () {
+        if (gameObject.GetComponent<CapsuleCollider2D>())
+            playerCollider = gameObject.GetComponent<CapsuleCollider2D>();
+        
+        else if (gameObject.GetComponent<Rigidbody2D>())
+            playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		forceY = Input.GetAxis("Vertical");
+		
+        playerRigidbody.AddForce(new Vector2(0, forceY));
+    }
+}
